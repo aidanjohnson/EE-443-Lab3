f0640367dc784fb651d522f206fcc48968f6ac43
Aidan Johnson <34667756+aidanjohnson@users.noreply.github.com>
2018-04-19 19:04:40 -0700

    function that returns MFCCs and plots

--- getMFCC.m
+++ getMFCC.m
@@ -1,22 +1,29 @@
-function [mfccs] = getMFCC(file)
-    %   Returns MFC coefficients, plots time (audio) signal and MFCC
+function [mfccs] = getMFCC(file, mkFig)
+    %   Returns MFC coefficients, and plots time (audio) signal and MFCC.
+    %   Plotting is optional (i.e., mkFig can be omitted).
+    %   If mkFig is TRUE, plots audio and MFCCs, else no plots by default.
     %
     %   Example call:
     %
-    %   close all; clear all; 
-    %   getMFCC('Duck.wav');
-    %   getMFCC('Bluejay.mp3');
-    %   getMFCC('Dove.mp3');
+    %   [mfccs_duck] = getMFCC('Duck.wav', 1);
+    %   [mfccs_bluejay] = getMFCC('Bluejay.mp3', 1);
+    %   [mfccs_dove] = getMFCC('Dove.mp3', 1);
     %
-    %   See: https://www.mathworks.com/matlabcentral/fileexchange/32849-htk-mfcc-matlab
-
+    %   Requires:
+    %   www.mathworks.com/matlabcentral/fileexchange/32849-htk-mfcc-matlab
+    
+    % Optional plot parameter set to default
+    if ~exist('mkFig','var')
+        mkFig = 0;
+    end
+    
     % Load audio signal
     [y, Fs] = audioread(file);
-    y = y(1:Fs*4,:);
+    % y = y(1:Fs*4,:); % only 4 s of sample
 
-    ch = size(y,2);
-    dur = size(y,1);
-    t = (0:dur-1)*(1/Fs);
+    ch = size(y,2); % number of audio channels
+    dur = size(y,1); % sample length of audio input
+    t = (0:dur-1)*(1/Fs); % time vector for file sampling frequency in Hz
     
     % Convert stereo to mono
     if (ch >= 2)
@@ -29,76 +36,42 @@ function [mfccs] = getMFCC(file)
     Tw = 21; % analysis frame duration (ms)
     Ts = 10; % analysis frame shift (ms) -- 10.48 for 380x13 4 s
     alpha = 0.97; % preemphasis coefficient
-    R = [300, 3700]; % frequency range to consider
+    R = [100, 10000]; % frequency range to consider (Hz)
     M = 20; % number of filterbank channels 
     C = 13; % number of cepstral coefficients
     L = 22; % cepstral sine lifter parameter
 
     % Create hamming window
     w = @(N)(0.54-0.46*cos(2*pi*(0:N-1).'/(N-1)));
-
-    % Plot speech samples given sampling rate
-    figure;
-    plot(t, y_mono(:));
-    title('Monaural ' + string(file) + ' (' + string(t(end)) + ' s)');
-    ylabel('amplitude');
-    xlabel('time [s]');
-    grid on;
     
     % Play speech samples
-    sound(y_mono, Fs);
+    % sound(y_mono, Fs);
 
     % Feature extraction (feature vectors as columns)
     [MFCC, FBE, frames] = mfcc(y_mono, Fs, Tw, Ts, alpha, w, R, M, C, L);
+    
+    if mkFig == 1
+        % Plot cepstrum over time
+        figure;
 
-    % Plot cepstrum over time
-    figure('Position', [30 100 800 200], 'PaperPositionMode', 'auto', ... 
-         'color', 'w', 'PaperOrientation', 'landscape', 'Visible', 'on' ); 
+        % Plot speech samples given sampling rate
+        subplot(2,1,1);
+        plot(t, y_mono(:));
+        title('Monaural ' + string(file) + ' (' + string(t(end)) + ' s)');
+        ylabel('amplitude');
+        xlabel('time [s]');
 
-    imagesc((1:size(MFCC,2)), (0:C-1), MFCC); 
-    axis('xy');
-    xlabel('frame index'); 
-    ylabel('cepstrum index');
-    dim = ': ' + string(size(MFCC,2)) + 'x' + string(size(MFCC,1));
-    name = string(file) + ' Mel-Frequency Cepstral Coefficients (MFCC)';
-    title(name + dim);
+        % Plot MFC coefficients
+        subplot(2,1,2);
+        imagesc((1:size(MFCC,2)), (0:C-1), MFCC); 
+        axis('xy');
+        xlabel('frame index'); 
+        ylabel('cepstrum index');
+        dim = ': ' + string(size(MFCC,2)) + 'x' + string(size(MFCC,1));
+        name = string(file) + ' Mel-Frequency Cepstral Coefficients (MFCC)';
+        title(name + dim);
+    end
     
     % MFC coefficients row vectors
     mfccs = MFCC';
 end
-%     Add below above, replacing plottin
-%
-%     % Generate plots
-%     figure('Position', [30 30 800 600], 'PaperPositionMode', 'auto', ... 
-%               'color', 'w', 'PaperOrientation', 'landscape', 'Visible', 'on' ); 
-% 
-%     subplot( 311 );
-%     plot( time, speech, 'k' );
-%     xlim( [ min(time_frames) max(time_frames) ] );
-%     xlabel( 'Time (s)' ); 
-%     ylabel( 'Amplitude' ); 
-%     title( 'Speech waveform'); 
-% 
-%     subplot( 312 );
-%     imagesc( time_frames, [1:M], logFBEs ); 
-%     axis( 'xy' );
-%     xlim( [ min(time_frames) max(time_frames) ] );
-%     xlabel( 'Time (s)' ); 
-%     ylabel( 'Channel index' ); 
-%     title( 'Log (mel) filterbank energies'); 
-% 
-%     subplot( 313 );
-%     imagesc( time_frames, [1:C], MFCCs(2:end,:) ); % HTK's TARGETKIND: MFCC
-%     %imagesc( time_frames, [1:C+1], MFCCs );       % HTK's TARGETKIND: MFCC_0
-%     axis( 'xy' );
-%     xlim( [ min(time_frames) max(time_frames) ] );
-%     xlabel( 'Time (s)' ); 
-%     ylabel( 'Cepstrum index' );
-%     title( 'Mel frequency cepstrum' );
-% 
-%     % Set color map to grayscale
-%     colormap( 1-colormap('gray') ); 
-% 
-%     % Print figure to pdf and png files
-%     print('-dpdf', sprintf('%s.pdf', mfilename)); 
-%     print('-dpng', sprintf('%s.png', mfilename));
