c67f2dd51f2264a8c794dd93ced965b021b0bad8
Aidan Johnson <aidanjohnson@github.com>
2018-04-24 12:28:04 -0700

    gmm_write.m *I think* now sends GMM in format expected by main.c

--- gmm_write.m
+++ gmm_write.m
@@ -1,12 +1,36 @@
-close all; close all;
+close all; clear all;
 delete(instrfindall);
 s = serial('COM7', 'BaudRate',115200);
 set(s,'InputBufferSize',1024);
 fopen(s);
 
 load('GMModel.mat');
- 
-fwrite(s, GMModel); 
+
+means = GMModel.mu;
+weights = GMModel.PComponents;
+vars = GMModel.Sigma;
+
+GMMmeans = zeros(1,size(means,1)*size(means,2));
+GMMvariances = zeros(1,size(vars,3)*size(vars,2));
+
+for k = 1:size(means,1)
+    GMMmeans(size(means,2)*(k-1)+1:size(means,2)*k) = means(k,:);
+end
+for n = 1:length(GMMmeans)
+    fwrite(s, GMMmeans(1,n));
+end
+
+GMMweights = weights;
+for n = 1:length(GMMweights)
+    fwrite(s, GMMweights(1,n));
+end
+
+for k = 1:size(vars,3)
+    GMMvariances(size(vars,2)*(k-1)+1:size(vars,2)*k) = diag(vars(:,:,k));
+end
+for n = 1:length(GMMvariances)
+    fwrite(s, GMMvariances(1,n));
+end
 
 fclose(s);
 delete(s);
