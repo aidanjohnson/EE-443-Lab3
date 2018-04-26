c2df530097a1c389f07e2087e272ea74e9d5cad3
aidanjohnson <j.a.johnson@ieee.org>
2018-04-25 18:44:50 -0700

    compiles by untested

---
+++ lab3_problem5/Debug/StartUp.d
@@ -0,0 +1,19 @@
+# FIXED
+
+StartUp.obj: ../StartUp.c
+StartUp.obj: ../DSP_Config.h
+StartUp.obj: ../LCDK_Support_DSP.h
+StartUp.obj: ../tistdtypes.h
+StartUp.obj: ../OMAPL138_defines.h
+StartUp.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/c6x.h
+StartUp.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/vect.h
+StartUp.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/stdint.h
+
+../StartUp.c: 
+../DSP_Config.h: 
+../LCDK_Support_DSP.h: 
+../tistdtypes.h: 
+../OMAPL138_defines.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/c6x.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/vect.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/stdint.h: 
---
+++ lab3_problem5/Debug/svm.d
@@ -0,0 +1,43 @@
+# FIXED
+
+svm.obj: ../svm.cpp
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/math.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/float.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/access.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/linkage.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/elfnames.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/mathf.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/unaccess.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/mathl.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/unaccess.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/stdio.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/stdarg.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/stdlib.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/ctype.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/_isfuncdcl.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/string.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/stdarg.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/limits.h
+svm.obj: /opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/locale.h
+svm.obj: ../svm.h
+
+../svm.cpp: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/math.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/float.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/access.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/linkage.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/elfnames.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/mathf.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/unaccess.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/mathl.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/unaccess.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/stdio.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/stdarg.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/stdlib.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/ctype.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/_isfuncdcl.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/string.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/stdarg.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/limits.h: 
+/opt/ti/ccsv6/tools/compiler/c6000_7.4.16/include/locale.h: 
+../svm.h: 
