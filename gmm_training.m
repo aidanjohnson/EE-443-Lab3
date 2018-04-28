a1ab673dc99700974772a6a6f0a3a75fe493b438
aidanjohnson <kahuna94>
2018-04-28 16:11:40 -0700

    updated parameters for SVM and GMM

--- gmm_training.m
+++ gmm_training.m
@@ -5,5 +5,11 @@ close all;
 [mfccs_bluejay] = getMFCC('Bluejay.mp3');
 [mfccs_dove] = getMFCC('Dove.mp3');
 
+save('mfccs_duck.mat','mfccs_duck');
+save('mfccs_bluejay.mat','mfccs_bluejay');
+save('mfccs_dove.mat','mfccs_dove');
+
 birds = [mfccs_duck; mfccs_bluejay; mfccs_dove];
 GMModel = fitgmdist(birds, 3);
+
+save('GMModel.mat','GMModel');
--- svm_training.m
+++ svm_training.m
@@ -5,6 +5,10 @@ close all;
 [mfccs_bluejay] = getMFCC('Bluejay.mp3');
 [mfccs_dove] = getMFCC('Dove.mp3');
 
+save('mfccs_duck.mat','mfccs_duck');
+save('mfccs_bluejay.mat','mfccs_bluejay');
+save('mfccs_dove.mat','mfccs_dove');
+
 labels_duck = repmat({'Duck'},size(mfccs_duck, 1),1);
 labels_bluejay = repmat({'Bluejay'},size(mfccs_bluejay, 1),1);
 labels_dove = repmat({'Dove'},size(mfccs_dove, 1),1);
@@ -14,3 +18,6 @@ birds = [mfccs_duck; mfccs_bluejay; mfccs_dove];
 SVMModel = fitcecoc(birds, labels);
 t = templateSVM('SaveSupportVectors', 'on');
 Md1 = fitcecoc(birds, labels, 'Learners', t);
+
+save('SVMModel.mat','SVMModel');
+save('Md1.mat','Md1');
